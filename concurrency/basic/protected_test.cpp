888e82bcec84341dbc739a9b5ae9351cd792d9f8
qinziwen <qinziwen2013@163.com>
2024-01-10 17:07:59 +0800

    Update concurrency/basic/protected_test.cpp

--- concurrency/basic/protected_test.cpp
+++ concurrency/basic/protected_test.cpp
@@ -18,14 +18,19 @@ public:
 
         CBase base;
         base.m_Public = 1;
-        // base.nProtected = 2;    // Cannot access directly
+        // base.m_Protected = 2;    // Cannot access directly
     }
 };
 
 int main() {
     CBase base;
     base.m_Public = 1;
-    // base.nProtected = 2;    // Cannot access directly
+    // base.m_Protected = 2;    // Cannot access directly
+
+    CBase* pBase = new CBase;
+    pBase->m_Public = 1;
+    // pBase->m_Protected = 2;    // Cannot access directly
+    delete pBase;
 
     return 0;
 }
