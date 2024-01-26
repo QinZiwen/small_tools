f3976fa4a92711596d0761184e4a2e81cf682645
qinziwen <qinziwen2013@163.com>
2024-01-26 23:11:58 +0800

    Add concurrency/basic/unique_lock_test.cpp

---
+++ concurrency/basic/unique_lock_test.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <mutex>
+#include <thread>
+
+class Test {
+public:
+    void fun() {
+        std::unique_lock<std::mutex> lck(m_mtx);
+        if (bool(lck)) {
+            std::cout << "lock succ: bool(lck)" << std::endl;
+        }
+
+        if ((bool)lck) {
+            std::cout << "lock succ: (bool)lck" << std::endl;
+        }
+
+        if (lck) {
+            std::cout << "lock succ: lck" << std::endl;
+        }
+    }
+
+private:
+    std::mutex m_mtx;
+};
+
+int main() {
+    Test test;
+    std::thread thread(&Test::fun, &test);
+    thread.join();
+    return 0;
+}
