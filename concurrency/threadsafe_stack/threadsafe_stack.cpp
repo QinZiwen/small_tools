95199aac245c47aa00764af1281ee34a37817391
qinziwen <qinziwen2013@163.com>
2024-04-06 22:40:35 +0800

    [feat] add threadsafe stack and queue

---
+++ concurrency/threadsafe_stack/threadsafe_stack.cpp
@@ -0,0 +1,37 @@
+#include "threadsafe_stack.h"
+
+#include <thread>
+#include <iostream>
+#include <random>
+
+ccy::ThreadsafeStack<int> g_safeStack;
+int main() {
+    std::vector<std::thread> threads;
+    std::random_device rd;
+    std::uniform_int_distribution<int> dist(100, 500);
+    for (int i = 0; i < 5; ++i) {
+        threads.emplace_back([&i, &rd, &dist]() {
+            std::cout << std::this_thread::get_id() << " running" << std::endl;
+            for (int j = 0; j < 3; ++j) {
+                g_safeStack.push(j * i);
+            }
+
+            std::cout << std::this_thread::get_id() << " popping" << std::endl;
+            while (!g_safeStack.empty()) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(dist(rd)));
+                try {
+                    std::cout << std::this_thread::get_id() << " : " << *(g_safeStack.pop()) << std::endl;
+                } catch (const ccy::EmptyStack& e) {
+                    std::cout << std::this_thread::get_id() << " : " << e.what() << std::endl;
+                    break;
+                }
+            }
+        });
+    }
+
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    return 0;
+}
---
+++ concurrency/threadsafe_stack/threadsafe_stack.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <exception>
+#include <memory>
+#include <mutex>
+#include <stack>
+
+namespace ccy {
+
+struct EmptyStack : public std::exception {
+    const char* what() const noexcept override {
+        return "Empty stack";
+    }
+};
+
+template <typename T>
+class ThreadsafeStack {
+public:
+    ThreadsafeStack() = default;
+    ThreadsafeStack(const ThreadsafeStack& other) {
+        std::lock_guard<std::mutex> lock(other.m_mutex);
+        m_data = other.m_data;
+    }
+    ThreadsafeStack& operator=(const ThreadsafeStack& other) = delete;
+
+    void push(T val) {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        m_data.push(val);
+    }
+
+    std::shared_ptr<T> pop() {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        if (m_data.empty()) {
+            throw EmptyStack();
+        }
+        std::shared_ptr<T> res(std::make_shared<T>(m_data.top()));
+        m_data.pop();
+        return res;
+    }
+
+    void pop(T& val) {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        if (m_data.empty()) {
+            throw EmptyStack();
+        }
+        val = m_data.top();
+        m_data.pop();
+    }
+
+    bool empty() const {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        return m_data.empty();
+    }
+
+private:
+    std::stack<T> m_data;
+    mutable std::mutex m_mutex;
+};
+
+}  // namespace ccy
