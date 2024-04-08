e63498a54bde82bbbc237750688f391f7f57dedd
qinziwen <qinziwen2013@163.com>
2024-04-08 22:05:37 +0800

    [feat] add concurrency/sort

---
+++ concurrency/sort/sequential_quick_sort.cpp
@@ -0,0 +1,27 @@
+#include "sequential_quick_sort.h"
+
+#include <iostream>
+
+int main() {
+    std::list<int> list = {4, 5, 1, 6, 9, 2, 7, 3, 8, 4, 5, 1, 6, 9, 2, 7, 3, 8};
+    list = ccy::sequentialQuickSort(list);
+    for (auto i : list) {
+        std::cout << i << " ";
+    }
+    std::cout << "\n--------" << std::endl;
+
+    list = {4, 5, 1, 6, 9, 2, 7, 3, 8, 4, 5, 1, 6, 9, 2, 7, 3, 8};
+    list = ccy::parallelQuickSort(list);
+    for (auto i : list) {
+        std::cout << i << " ";
+    }
+    std::cout << "\n--------" << std::endl;
+
+    list = {4, 5, 1, 6, 9, 2, 7, 3, 8, 4, 5, 1, 6, 9, 2, 7, 3, 8};
+    list = ccy::parallelQuickSortSpawnTask(list);
+    for (auto i : list) {
+        std::cout << i << " ";
+    }
+    std::cout << "\n--------" << std::endl;
+    return 0;
+}
