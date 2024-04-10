c5f3daf8db82a6714f231adddea98c559064457e
qinziwen <qinziwen2013@163.com>
2024-04-10 22:50:14 +0800

    [feat] add concurrency/atm

---
+++ concurrency/atm/atm_main.cpp
@@ -0,0 +1,62 @@
+#include "teller.h"
+
+#include <thread>
+
+using namespace ccy;
+
+int main() {
+    std::cout << ">>>>>> ATM <<<<<<" << std::endl;
+
+    BankMaching bank;
+    InterfaceMachine interface_hardware;
+    Atm machine(bank.getSender(), interface_hardware.getSender());
+
+    std::thread bank_thread(&BankMaching::run, &bank);
+    std::thread if_thread(&InterfaceMachine::run, &interface_hardware);
+    std::thread atm_thread(&Atm::run, &machine);
+
+    Sender atmqueue(machine.getSender());
+    bool quit_pressed = false;
+    while (!quit_pressed) {
+        char c = getchar();
+        switch (c) {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            atmqueue.send(DigitPressed(c));
+            break;
+        case 'b':
+            atmqueue.send(BalancePressed());
+            break;
+        case 'w':
+            atmqueue.send(Withdraw_pressed(50));
+            break;
+        case 'c':
+            atmqueue.send(CancelPressed());
+            break;
+        case 'q':
+            quit_pressed = true;
+            break;
+        case 'i':
+            atmqueue.send(CardInserted("account123456"));
+            break;
+        }
+    }
+
+    bank.done();
+    machine.done();
+    interface_hardware.done();
+
+    bank_thread.join();
+    if_thread.join();
+    atm_thread.join();
+
+    return 0;
+}
---
+++ concurrency/atm/template_dispatcher.h
@@ -0,0 +1,77 @@
+#pragma once
+
+#include "message.h"
+
+namespace ccy {
+
+template <typename PreviousDispatcher, typename Msg, typename Func>
+class TemplateDispatcher {
+public:
+    // TemplateDispatcher的各种特化版本互为友元
+    template <typename D, typename M, typename F>
+    friend class TemplateDispatcher;
+
+public:
+    TemplateDispatcher(const TemplateDispatcher&) = delete;
+    TemplateDispatcher& operator=(const TemplateDispatcher&) = delete;
+    TemplateDispatcher(TemplateDispatcher&& other)
+        : m_queue(other.m_queue),
+          m_prev(other.m_prev),
+          m_func(std::move(other.m_func)),
+          m_chained(other.m_chained) {
+        other.m_chained = true;
+    }
+    TemplateDispatcher(Queue* q, PreviousDispatcher* prev, Func&& func)
+        : m_queue(q),
+          m_prev(prev),
+          m_func(std::forward<Func>(func)),
+          m_chained(false) {
+        prev->m_chained = true;
+        std::cout << "[TemplateDispatcher::TemplateDispatcher] queue_address: " << m_queue << std::endl;
+    }
+    ~TemplateDispatcher() noexcept(false) {
+        std::cout << "TemplateDispatcher::~TemplateDispatcher m_chained: " << m_chained << std::endl;
+        if (!m_chained) {
+            waitAndDispatch();
+        }
+    }
+
+    template <typename OtherMsg, typename OtherFunc>
+    TemplateDispatcher<TemplateDispatcher, OtherMsg, OtherFunc> handle(OtherFunc&& of) {
+        std::cout << "[TemplateDispatcher::handle] OtherMsg: " << typeid(OtherMsg).name()
+                  << " OtherFunc: " << typeid(OtherFunc).name() << std::endl;
+        return TemplateDispatcher<TemplateDispatcher, OtherMsg, OtherFunc>(m_queue, this, std::forward<OtherFunc>(of));
+    }
+
+private:
+    void waitAndDispatch() {
+        std::cout << "[TemplateDispatcher::waitAndDispatch]" << std::endl;
+        while (true) {
+            auto msg = m_queue->waitAndPop();
+            if (dispatch(msg)) {
+                break;
+            }
+        }
+        std::cout << "[TemplateDispatcher::waitAndDispatch] done" << std::endl;
+    }
+
+    bool dispatch(const std::shared_ptr<MessageBase>& msg) {
+        std::cout << "[TemplateDispatcher::dispatch]" << std::endl;
+        if (WrappedMessage<Msg>* wrapper = dynamic_cast<WrappedMessage<Msg>*>(msg.get())) {
+            std::cout << "[TemplateDispatcher::dispatch] wrapper: " << wrapper << std::endl;
+            m_func(wrapper->contents);
+            return true;
+        } else {
+            std::cout << "[TemplateDispatcher::dispatch] wrapper: nullptr" << std::endl;
+            return m_prev->dispatch(msg);
+        }
+    }
+
+private:
+    Queue* m_queue;
+    PreviousDispatcher* m_prev;
+    Func m_func;
+    bool m_chained;
+};
+
+}  // namespace ccy
