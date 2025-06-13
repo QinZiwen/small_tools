8d098d8cafa57cd66fe5b69e557bd54c8796fcee
qinziwen <qinziwen2013@163.com>
2025-06-13 17:19:16 +0800

    [feature] add fnction_wrapper.cpp

---
+++ algorithm_practice/gdbTest.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int fun(int a) {
+	std::cout << "a: " << a << std::endl;
+	int res = a;
+	for (int i = 0; i < a; ++i) {
+		res += a;
+		std::cout << "res: " << res <<std::endl;
+	}
+	return res;
+}
+
+int main() {
+	int a = 5;
+	std::cout << "a: " << a <<std::endl;
+	a = fun(a);
+
+	int b = a;
+	std::cout << "b: " << b << std::endl;
+	return 0;
+}
