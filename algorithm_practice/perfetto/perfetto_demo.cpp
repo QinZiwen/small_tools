2ea9d711a039aa4c07d0596099eca0b5d8a6a571
qinziwen <qinziwen2013@163.com>
2025-09-11 11:43:22 +0800

    [feat] add perfetto

---
+++ algorithm_practice/perfetto/perfetto_demo.cpp
@@ -0,0 +1,30 @@
+#include <perfetto.h>
+
+PERFETTO_DEFINE_CATEGORIES(
+    perfetto::Category("rendering")
+        .SetDescription("Events from the graphics subsystem"),
+    perfetto::Category("network")
+        .SetDescription("Network upload and download statistics"));
+
+PERFETTO_TRACK_EVENT_STATIC_STORAGE();
+
+int main(int argc, char** argv) {
+  perfetto::TracingInitArgs args;
+
+  // The backends determine where trace events are recorded. You may select one
+  // or more of:
+
+  // 1) The in-process backend only records within the app itself.
+  args.backends |= perfetto::kInProcessBackend;
+
+  // 2) The system backend writes events into a system Perfetto daemon,
+  //    allowing merging app and system events (e.g., ftrace) on the same
+  //    timeline. Requires the Perfetto `traced` daemon to be running (e.g.,
+  //    on Android Pie and newer).
+  args.backends |= perfetto::kSystemBackend;
+
+  perfetto::Tracing::Initialize(args);
+  perfetto::TrackEvent::Register();
+
+  return 0;
+}
